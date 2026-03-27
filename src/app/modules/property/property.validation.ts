a4a9b3a42f950f6acda59220017d991fe2644fd0
monir6163 <monirhossain6163@gmail.com>
2026-03-27 09:26:49 +0600

    feat: property-zod-validation

---
+++ src/app/modules/property/property.validation.ts
@@ -0,0 +1,50 @@
+import * as z from "zod";
+import {
+  ListingType,
+  PropertyStatus,
+  PropertyType,
+} from "../../../generated/prisma/enums";
+
+export const PropertyValidation = {
+  propertyCreateSchema: z.object({
+    body: z.object({
+      title: z.string().min(1, "Title is required"),
+      description: z.string().min(1, "Description is required"),
+      price: z.number().positive("Price must be a positive number"),
+      location: z.string().min(1, "Location is required"),
+      address: z.string().optional(),
+      thumbnail: z.string().min(1, "Thumbnail URL is required"),
+      bedrooms: z
+        .number()
+        .int()
+        .nonnegative("Bedrooms must be a non-negative integer"),
+      bathrooms: z
+        .number()
+        .int()
+        .nonnegative("Bathrooms must be a non-negative integer"),
+      area: z.number().positive("Area must be a positive number"),
+      type: z.enum(
+        [
+          PropertyType.APARTMENT,
+          PropertyType.HOUSE,
+          PropertyType.COMMERCIAL,
+          PropertyType.LAND,
+        ],
+        {
+          message: "Invalid property type",
+        },
+      ),
+      listingType: z.enum([ListingType.RENT, ListingType.SALE], {
+        message: "Invalid listing type",
+      }),
+      status: z.enum(
+        [PropertyStatus.AVAILABLE, PropertyStatus.RENTED, PropertyStatus.SOLD],
+        {
+          message: "Invalid property status",
+        },
+      ),
+      isPremium: z.boolean(),
+      isFeatured: z.boolean(),
+    }),
+  }),
+};
